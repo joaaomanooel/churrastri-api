de3017d363cec5b3291bedbccd471f02b2e99c5b
João Manoel Neto <joaaomanooel@gmail.com>
2020-02-04 01:34:37 -0300

    update dotenv

--- src/app/routes/index.js
+++ src/app/routes/index.js
@@ -7,7 +7,7 @@ const router = Router();
 
 fs
   .readdirSync(__dirname)
-  .filter((file) => ((file.indexOf('.')) !== 0 && (file !== 'index.js')))
-  .forEach((file) => router.use(require(path.resolve(__dirname, file))));
+  .filter(file => ((file.indexOf('.')) !== 0 && (file !== 'index.js')))
+  .forEach(file => router.use(require(path.resolve(__dirname, file))));
 
 module.exports = router;
