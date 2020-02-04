8a7dd4f6b1e0b191b47b7c6fc69ac4db567071ed
João Manoel Neto <joaaomanooel@gmail.com>
2020-02-04 02:54:30 -0300

    update user routes

--- src/app/routes/users.js
+++ src/app/routes/users.js
@@ -2,9 +2,11 @@ const { Router } = require('express');
 const controller = require('../controllers/users');
 const authMiddleware = require('../middleware/auth');
 
+const baseUrl = '/users';
+
 module.exports = Router()
-  .get('/users', controller.getAll)
-  .post('/users', controller.insert)
-  .get('/users/:id', controller.getById)
-  .put('/users/:id', authMiddleware, controller.update)
-  .delete('/users/:id', authMiddleware, controller.remove);
+  .get(baseUrl, controller.getAll)
+  .post(baseUrl, controller.insert)
+  .get(`${baseUrl}/:id`, controller.getById)
+  .put(`${baseUrl}/:id`, authMiddleware, controller.update)
+  .delete(`${baseUrl}/:id`, authMiddleware, controller.remove);
